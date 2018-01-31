4e30bef8a99e7a859606ab2a0611166db02b62b5
Matthew O'Hare <matthewohare1@aol.com>
2018-01-31 13:19:56 +0000

    eighth commit

--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,13 @@ from rango import views
 
 urlpatterns = [
 url(r'^$', views.index, name='index'),
-url(r'$', views.about, name='about'),
+
+url(r'^about/', views.about, name='about'),
+
+url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+views.show_category, name='show_category'),
+
+url(r'^mostViewed/(?P<category_name_slug>[\w\-]+)/$',
+views.show_category, name='most_viewed'),
+
 ]
