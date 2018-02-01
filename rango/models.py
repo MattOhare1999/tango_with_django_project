3eddd51ab7b40d62bd429aee4f5ba1a3a7ae5608
Matthew O'Hare <matthewohare1@aol.com>
2018-02-01 15:03:28 +0000

    ninth commit

--- rango/models.py
+++ rango/models.py
@@ -5,6 +5,7 @@ from django.template.defaultfilters import slugify
 # Create your models here.
 
 class Category(models.Model):
+
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
--- rango/urls.py
+++ rango/urls.py
@@ -9,7 +9,9 @@ url(r'^about/', views.about, name='about'),
 url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
 views.show_category, name='show_category'),
 
-url(r'^mostViewed/(?P<category_name_slug>[\w\-]+)/$',
-views.show_category, name='most_viewed'),
+url(r'^add_category/$', views.add_category, name='add_category'),
+
+url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
+
 
 ]
